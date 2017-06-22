70b8ea6d8b4778cbe725fa81b1dc634f6c23d0fc
vishal <vishal@flotomate.com>
2017-06-23 01:17:50 +0530

    added table component

--- App.jsx
+++ App.jsx
@@ -1,13 +1,77 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World, Im Vishal!!!
-         </div>
-      );
-   }
+
+    constructor(){
+        super();
+
+        this.state = {
+            data:[
+                {
+                    "id":1,
+                    "name":"vishal"
+                },
+                {
+                    "id":2,
+                    "name":"krinal"
+                },
+                {
+                    "id":3,
+                    "name":"nivaan"
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Table data={this.state.data} border={1}/>
+            </div>
+        );
+    }
 }
+
+class Table extends React.Component{
+    render(){
+        return(
+            <table>
+                <tbody>
+                    {this.props.data.map((person,i)=> <TableRow key={i} data = {person} />)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class TableRow extends React.Component{
+    render(){
+        return(
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+            </tr>
+        );
+    }
+}
+class Header extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Header Content</h1>
+            </div>
+        );
+    }
+}
+
+
+// class Content extends React.Component {
+//     render() {
+//         return(
+//             <div>Content data</div>
+//         );
+//     }
+// }
 
 export default App;
